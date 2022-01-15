3c4280a8998cd61e6435f7482a826a20e9e8b1c7
Rinzer1 <csxa_@outlook.com>
2022-01-15 12:17:32 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './login';
+import Login from './login';
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Login;
